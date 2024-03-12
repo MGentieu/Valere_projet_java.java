c73e0fab5d1f5243ff31b7d46e49134cbff796bb
MGentieu <mgentieu02@gmail.com>
2024-03-12 19:46:51 +0100

    ezfzf

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        try {
+            // Création d'une nouvelle connexion à la base de données
+            Connexion connexion = new Connexion("ece_shopping", "root", "");
+            //u.validerPanier();
+            /*
+            String requete = "UPDATE livre SET stock = 3 WHERE identifiant = 1;";
+            connexion.executeUpdate(requete);
+            requete = "UPDATE livre SET stock = 3 WHERE identifiant = 2;";
+            connexion.executeUpdate(requete);
+            requete = "SELECT * FROM client WHERE id = 1;";
+            */
+
+            connexion.close();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
